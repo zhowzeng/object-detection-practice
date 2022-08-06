63b5ea7621e8eab387fbb2061a39f444c78112b6
zhow <zenglihao106@gmail.com>
2022-08-06 16:50:34 +0800

    chore: clean comments

--- train.py
+++ train.py
@@ -42,8 +42,6 @@ def generate_target(image_id: int, fpath: Path):
         soup = BeautifulSoup(data, 'xml')
         objects = soup.find_all('object')
 
-        num_objs = len(objects)
-
         boxes = []
         labels = []
         for i in objects:
@@ -145,22 +143,17 @@ def get_transform(train):
     return T.Compose(transforms)
 
 
-# train on the GPU or on the CPU, if a GPU is not available
 # device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 device = torch.device('cpu')
 
-# our dataset has two classes only - background and person
 num_classes = 2
-# use our dataset and defined transformations
 dataset = FaceMaskDataset('./data/face-mask/', get_transform(train=True))
 dataset_test = FaceMaskDataset('./data/face-mask/', get_transform(train=False))
 
-# split the dataset in train and test set
 indices = torch.randperm(len(dataset)).tolist()
 dataset = torch.utils.data.Subset(dataset, indices[:-50])
 dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
-# define training and validation data loaders
 data_loader = torch.utils.data.DataLoader(
     dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=collate_fn
 )
@@ -169,23 +162,20 @@ data_loader_test = torch.utils.data.DataLoader(
     dataset_test, batch_size=1, shuffle=False, num_workers=4, collate_fn=collate_fn
 )
 
-# get the model using our helper function
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
     weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT
 )
-num_classes = 3  # 1 class (person) + background
+num_classes = 3
 in_features = model.roi_heads.box_predictor.cls_score.in_features
 model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-# move model to the right device
 model.to(device)
-# construct an optimizer
+
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-# and a learning rate scheduler
+
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
-# let's train it for 10 epochs
 logging.info('start training')
 num_epochs = 1
 for epoch in range(num_epochs):
