b28dbc4b376a1af634d8f6d8058a52f14e60b8b5
zhow <zenglihao106@gmail.com>
2022-08-06 18:28:28 +0800

    refactor: pl level 1

--- train.py
+++ train.py
@@ -1,19 +1,15 @@
 import logging
-import math
-import sys
 from pathlib import Path
 
+import pytorch_lightning as pl
 import torch
+import torch.nn.functional as F
 import torchvision
+import torchvision.transforms as T
 from bs4 import BeautifulSoup
 from PIL import Image
-from torchvision import transforms as T
-from torchvision.models.detection.faster_rcnn import (
-    FasterRCNN_ResNet50_FPN_Weights,
-    FastRCNNPredictor,
-)
-
-from utils import MetricLogger, SmoothedValue, collate_fn, reduce_dict
+from torch import nn
+from torchvision.models.detection import faster_rcnn, fasterrcnn_resnet50_fpn
 
 logging.basicConfig(level=logging.INFO)
 
@@ -83,55 +79,8 @@ class FaceMaskDataset(torch.utils.data.Dataset):
         return len(self.imgs)
 
 
-def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, scaler=None):
-    model.train()
-    metric_logger = MetricLogger(delimiter="  ")
-    metric_logger.add_meter("lr", SmoothedValue(window_size=1, fmt="{value:.6f}"))
-    header = f"Epoch: [{epoch}]"
-
-    lr_scheduler = None
-    if epoch == 0:
-        warmup_factor = 1.0 / 1000
-        warmup_iters = min(1000, len(data_loader) - 1)
-
-        lr_scheduler = torch.optim.lr_scheduler.LinearLR(
-            optimizer, start_factor=warmup_factor, total_iters=warmup_iters
-        )
-
-    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
-        images = list(image.to(device) for image in images)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        with torch.cuda.amp.autocast(enabled=scaler is not None):
-            loss_dict = model(images, targets)
-            losses = sum(loss for loss in loss_dict.values())
-
-        # reduce losses over all GPUs for logging purposes
-        loss_dict_reduced = reduce_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
-        loss_value = losses_reduced.item()
-
-        if not math.isfinite(loss_value):
-            print(f"Loss is {loss_value}, stopping training")
-            print(loss_dict_reduced)
-            sys.exit(1)
-
-        optimizer.zero_grad()
-        if scaler is not None:
-            scaler.scale(losses).backward()
-            scaler.step(optimizer)
-            scaler.update()
-        else:
-            losses.backward()
-            optimizer.step()
-
-        if lr_scheduler is not None:
-            lr_scheduler.step()
-
-        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
-        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-
-    return metric_logger
+def collate_fn(batch):
+    return tuple(zip(*batch))
 
 
 def get_transform(train):
@@ -143,10 +92,31 @@ def get_transform(train):
     return T.Compose(transforms)
 
 
-# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-device = torch.device('cpu')
+class LitFasterRCNN(pl.LightningModule):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
 
-num_classes = 2
+    def training_step(self, batch, batch_idx):
+        images, targets = batch
+        images = list(image for image in images)
+        targets = [{k: v for k, v in t.items()} for t in targets]
+        loss_dict = self.model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+        return losses
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
+        return optimizer
+
+
+def generate_faster_rcnn_model(num_classes):
+    model = fasterrcnn_resnet50_fpn(weights=faster_rcnn.FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = faster_rcnn.FastRCNNPredictor(in_features, num_classes)
+    return model
+
+
 dataset = FaceMaskDataset('./data/face-mask/', get_transform(train=True))
 dataset_test = FaceMaskDataset('./data/face-mask/', get_transform(train=False))
 
@@ -154,36 +124,32 @@ indices = torch.randperm(len(dataset)).tolist()
 dataset = torch.utils.data.Subset(dataset, indices[:-50])
 dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
-data_loader = torch.utils.data.DataLoader(
+train_loader = torch.utils.data.DataLoader(
     dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=collate_fn
 )
 
-data_loader_test = torch.utils.data.DataLoader(
+test_loader = torch.utils.data.DataLoader(
     dataset_test, batch_size=1, shuffle=False, num_workers=4, collate_fn=collate_fn
 )
 
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
-    weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT
-)
-num_classes = 3
-in_features = model.roi_heads.box_predictor.cls_score.in_features
-model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+# model
+model = LitFasterRCNN(generate_faster_rcnn_model(num_classes=3))
 
-model.to(device)
+# train model
+trainer = pl.Trainer(accelerator='cpu')
+trainer.fit(model=model, train_dataloaders=train_loader)
 
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
 
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+# lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
-logging.info('start training')
-num_epochs = 1
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-    # update the learning rate
-    lr_scheduler.step()
-    # evaluate on the test dataset
-    # evaluate(model, data_loader_test, device=device)
+# logging.info('start training')
+# num_epochs = 1
+# for epoch in range(num_epochs):
+#     # train for one epoch, printing every 10 iterations
+#     train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+#     # update the learning rate
+#     lr_scheduler.step()
+#     # evaluate on the test dataset
+#     # evaluate(model, data_loader_test, device=device)
 
-print("That's it!")
+# print("That's it!")
